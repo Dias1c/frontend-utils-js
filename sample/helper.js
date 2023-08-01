dfee42d797985617f38e72fdfcfbd2dd268c3f48
Dias1c <dias.kappasov@gmail.com>
2023-08-01 19:15:16 +0600

    upd: helpers.js

--- sample/helper.js
+++ sample/helper.js
@@ -1,54 +1,54 @@
 const select_animation = "run-select-animation";
 // ? TAG Editor
-const tagEditor = exampleTagEditor
+const tagEditor = exampleTagEditor;
 const TagEditor_HasDoubles = document.getElementById("TagEditor_HasDoubles");
 
 // For Reset tags
 function ResetTagEditor() {
-    let tags = tagEditor.STags
-    tagEditor.STags = []
-    tags.forEach((tagname) => {
-        tagEditor.AddTag(tagname);
-    });
+  let tags = tagEditor.Tags;
+  tagEditor.Tags = [];
+  tags.forEach((tagname) => {
+    tagEditor.AddTag(tagname);
+  });
 
-    tagEditor.B_TagEditor.classList.remove(select_animation);
-    tagEditor.B_TagEditor.offsetWidth;
-    tagEditor.B_TagEditor.classList.add(select_animation);
+  tagEditor.domElement_TagEditorDiv.classList.remove(select_animation);
+  tagEditor.domElement_TagEditorDiv.offsetWidth;
+  tagEditor.domElement_TagEditorDiv.classList.add(select_animation);
 }
 
-TagEditor_HasDoubles.addEventListener('change', (e) => {
-    tagEditor.SHasDoubles = e.target.checked;
-    ResetTagEditor();
+TagEditor_HasDoubles.addEventListener("change", (e) => {
+  tagEditor.config.HasDoubles = e.target.checked;
+  ResetTagEditor();
 });
-
 
 const TagEditor_ToLower = document.getElementById("TagEditor_ToLower");
 
-TagEditor_ToLower.addEventListener('change', (e) => {
-    tagEditor.SToLower = e.target.checked;
-    ResetTagEditor();
+TagEditor_ToLower.addEventListener("change", (e) => {
+  tagEditor.config.ToLower = e.target.checked;
+  ResetTagEditor();
 });
-
 
 const TagEditor_MaxTags = document.getElementById("TagEditor_MaxTags");
 
-TagEditor_MaxTags.addEventListener('change', (e) => {
-    tagEditor.SMaxTags = e.target.value;
-    ResetTagEditor();
+TagEditor_MaxTags.addEventListener("change", (e) => {
+  tagEditor.config.MaxTagsCount = e.target.value;
+  ResetTagEditor();
 });
 
-const TagEditorFormContainer = document.getElementById("TagEditorFormContainer");
+const TagEditorFormContainer = document.getElementById(
+  "TagEditorFormContainer"
+);
 let TagEditorMethodRadioButtons = document.getElementsByName("TagEditorMethod");
 let TagEditorEndpoint_tb = document.getElementById("TagEditorEndpoint");
 
-TagEditorMethodRadioButtons.forEach((rb)=>{
-    rb.addEventListener('change', (e) => {
-        if (e.target.checked) {
-            TagEditorFormContainer.setAttribute("method", e.target.value);
-        }
-    })
+TagEditorMethodRadioButtons.forEach((rb) => {
+  rb.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      TagEditorFormContainer.setAttribute("method", e.target.value);
+    }
+  });
 });
 
 TagEditorEndpoint_tb.addEventListener("change", (e) => {
-    TagEditorFormContainer.setAttribute("action", e.target.value);
+  TagEditorFormContainer.setAttribute("action", e.target.value);
 });
